babe01387328412795b32529490be0a37899c64f
Dawid Jakobczak <dawidjakobczakbn@gmail.com>
2020-10-22 00:11:48 +0200

    exceptions: add custom exception template
    Add html template for exception handling

--- server/exceptions.py
+++ server/exceptions.py
@@ -1,7 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 
 class AnnotationException(Exception):
+    pass
+
+
+class ActionLockedException(Exception):
+    pass
+
+
+class FileException(Exception):
     pass
 
 
@@ -12,4 +20,17 @@ PLAIN = 'ContentType: text/plain'
 
 @errors.app_errorhandler(AnnotationException)
 def handle_annotations(error):
-    return str(error), 400
+    return render_template(
+        "error.html.j2", code=400, msg=str(error)), 400
+
+
+@errors.app_errorhandler(ActionLockedException)
+def handle_lock(error):
+    return render_template(
+        "error.html.j2", code=409, msg=str(error)), 409
+
+
+@errors.app_errorhandler(FileException)
+def handle_currupted_fille(error):
+    return render_template(
+        "error.html.j2", code=400, msg=str(error)), 400
