985f7ca8d10e0d8aff5c209bb2c61203e802862f
Dawid Jakobczak <dawidjakobczakbn@gmail.com>
2020-11-12 22:47:50 +0100

    training stats backend

--- server/views/train.py
+++ server/views/train.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from flask import flash, render_template
 
 from dral.logger import LOG
@@ -18,17 +20,18 @@ class TrainView(ActionView):
 
     def search(self, nepochs, reverse):  # !TODO refactoring unclear flow
         train_results = load_json(self.cm.get_train_results_file())
-
-        if not train_results:
+        show_results = self.is_dict_empty(train_results)
+        if show_results:
             flash('Training history is empty, '
                   'you have to train your model firstly', 'danger')
             return render_template(
-                'train.html.j2', results=dict())
+                'train.html.j2', results=dict(), show_results=False)
         results = self._get_last_n_results(train_results, nepochs, reverse)
-
+        stats = self._get_training_stats(results)
         return render_template(
             'train.html.j2',
-            show_results=True if train_results else False,
+            show_results=True,
+            stats=stats,
             results=zip(results['train_acc'],
                         results['train_loss'],
                         results['val_acc'],
@@ -50,3 +53,50 @@ class TrainView(ActionView):
         n = max(0, n)
         return {key: val[:n] for key, val in results.items()} if reverse \
             else {key: val[-n:] for key, val in results.items()}
+
+    def _get_training_stats(self, results):
+        tacc = results.get('train_acc')
+        tacc_epoch = np.argmax(tacc)
+        tacc = tacc[tacc_epoch]
+        tacc_description = \
+            f'Maximum training accuracy achieved on {tacc_epoch + 1} epoch'
+
+        tloss = results.get('train_loss')
+        tloss_epoch = np.argmin(tloss)
+        tloss = tloss[tloss_epoch]
+        tloss_description = \
+            f'Minimum training loss achieved on {tloss_epoch + 1} epoch'
+
+        vacc = results.get('val_acc')
+        vacc_epoch = np.argmax(vacc)
+        vacc = vacc[vacc_epoch]
+        vacc_description = \
+            f'Maximum validation accuracy achieved on {vacc_epoch + 1} epoch'
+
+        vloss = results.get('val_loss')
+        vloss_epoch = np.argmin(vloss)
+        vloss = vloss[vloss_epoch]
+        vloss_description = \
+            f'Minimum validation loss achieved on {vloss_epoch + 1} epoch'
+
+        return {
+            'tacc': tacc,
+            'tacc_epoch': tacc_epoch + 1,
+            'tacc_description': tacc_description,
+            'tloss': tloss,
+            'tloss_epoch': tloss_epoch + 1,
+            'tloss_description': tloss_description,
+            'vacc': vacc,
+            'vacc_epoch': vacc_epoch + 1,
+            'vacc_description': vacc_description,
+            'vloss': vloss,
+            'vloss_epoch': vloss_epoch + 1,
+            'vloss_description': vloss_description,
+        }
+
+    @staticmethod
+    def is_dict_empty(d):
+        for key, values in d.items():
+            if len(values):
+                return False
+        return True
